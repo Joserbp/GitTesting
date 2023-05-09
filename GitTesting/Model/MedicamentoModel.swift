287b444d34d300b282b69bcd594d4ff6d195ba46
OscarMtzGlz <o.martinez.gonzalez62@gmail.com>
2023-05-09 17:16:51 -0600

    Medicamento Model

---
+++ GitTesting/Model/MedicamentoModel.swift
@@ -0,0 +1,16 @@
+//
+//  MedicamentoModel.swift
+//  GitTesting
+//
+//  Created by MacBookMBA2 on 09/05/23.
+//
+
+import Foundation
+
+class MedicamentoModel {
+    
+    func GetAll() {
+        
+    }
+    
+}
