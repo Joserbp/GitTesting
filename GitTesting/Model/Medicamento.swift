75fac9f9cc727be8152083e6a5c92f7e2ff729c9
AdrianAriaMartinez <adriantarias97@gmail.com>
2023-05-09 18:00:25 -0600

    Creación de los modelos Medicamento y Proveedor

--- GitTesting/Model/Medicamento.swift
+++ GitTesting/Model/Medicamento.swift
@@ -7,7 +7,46 @@
 
 import Foundation
 
-class Medicamento{
-    var Id: Int = 0
-    let Nombre : String? = nil
+struct Medicamento {
+    
+    let IdMedicamento: Int
+    let Nombre: String
+    let Descripcion: String
+    let Precio: Double
+    let Imagen: String
+    let IdProveedor: Proveedor?
+    
+    init(IdMedicamento: Int, Nombre: String, Descripcion: String, Precio: Double, Imagen: String, IdProveedor: Proveedor?) {
+        self.IdMedicamento = IdMedicamento
+        self.Nombre = Nombre
+        self.Descripcion = Descripcion
+        self.Precio = Precio
+        self.Imagen = Imagen
+        self.IdProveedor = IdProveedor
+    }
+    init(){
+        self.IdMedicamento = 0
+        self.Nombre = ""
+        self.Descripcion = ""
+        self.Precio = 0.0
+        self.Imagen = ""
+        self.IdProveedor = nil
+    }
+    
+}
+
+struct Proveedor {
+    
+    let IdProveedor: Int
+    let Nombre: String
+    
+    init(IdProveedor: Int, Nombre: String){
+        self.IdProveedor = IdProveedor
+        self.Nombre = Nombre
+    }
+    
+    init(){
+        self.IdProveedor = 0
+        self.Nombre = ""
+    }
 }
