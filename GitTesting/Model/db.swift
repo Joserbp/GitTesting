ca55dabd213457ca70899b8d9e7ec9aa1a116ebe
OscarMtzGlz <o.martinez.gonzalez62@gmail.com>
2023-05-10 13:57:14 -0600

    conexion bd

--- GitTesting/Model/db.swift
+++ GitTesting/Model/db.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SQLite3
+
+class DB{
+    let Path: String = "DB.sql"
+    var db: OpaquePointer? = nil
+    
+    init(){
+        OpenConexion()
+    }
+    
+    func OpenConexion(){
+        let FilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathExtension(self.Path)
+        let db = sqlite3_open (FilePath.path,&db)
+        var opaquepointer: OpaquePointer? = nil
+        if db == SQLITE_OK{
+            print("Conexion Correcta")
+            print(FilePath)
+        }else{
+            print("Error al conectar con la BD")
+        }
+    }
+}
